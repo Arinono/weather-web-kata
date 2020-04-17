483e887e780adbfa0d5a9302e2d5034b531cf8ef
Aurelien Arino <aurelien.arino@gmail.com>
2020-04-17 15:52:15 +0200

    temperature

---
+++ src/components/Temperature.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <img :src="icon">
+      <p>{{ season }}</p>
+    </div>
+    <div />
+    <div>{{ temp }}<sup>º</sup></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'Temperature'
+})
+export default class Temperature extends Vue {
+  @Prop({ type: Number, required: true }) readonly temp!: number
+  @Prop({ type: String, required: true, validator: v => ['winter'].includes(v) }) readonly season!: string
+
+  private get icon (): string {
+    return this.season === 'winter' ? '@/assets/img/snowflake.png' : '@/assets/img/snowflake.png'
+  }
+}
+</script>
---
+++ tests/unit/components/Temperature.spec.ts
@@ -0,0 +1,30 @@
+import { shallowMount, Wrapper } from '@vue/test-utils'
+import Temperature from '@/components/Temperature.vue'
+
+describe('Temperature', () => {
+  let view: Wrapper<Temperature>
+  let vm: any
+
+  beforeAll(() => {
+    view = shallowMount(Temperature, {
+      propsData: {
+        temp: 1,
+        season: 'winter'
+      }
+    })
+    vm = (view as any).vm
+  })
+
+  afterAll(() => {
+    view.destroy()
+  })
+
+  it('should have default props', () => {
+    expect(vm.temp).toEqual(1)
+    expect(vm.season).toEqual('winter')
+  })
+
+  it('should diplay the snowflake icon', () => {
+    expect(vm.icon).toEqual('@/assets/img/snowflake.png')
+  })
+})

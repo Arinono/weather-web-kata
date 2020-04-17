cd575ef6e3fac90732b817d3782063aacedfcf8e
Aurelien Arino <aurelien.arino@gmail.com>
2020-04-17 15:38:46 +0200

    landscape

---
+++ src/components/Landscape.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="img">
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'Landscape'
+})
+export default class Landscape extends Vue {
+  @Prop({ type: String, default: 'fall', validator: v => ['fall', 'winter'].includes(v) }) readonly season!: string
+
+  private get img (): string {
+    return this.season === 'fall' ? '@/assets/img/illustration_1.jpg' : this.season === 'winter' ? '@/assets/img/illustration_2.jpg' : '@/assets/img/illustration_1.jpg'
+  }
+}
+</script>
---
+++ tests/unit/components/Landscape.spec.ts
@@ -0,0 +1,40 @@
+import { shallowMount, Wrapper } from '@vue/test-utils'
+import Landscape from '@/components/Landscape.vue'
+
+describe('Landscape', () => {
+  let view: Wrapper<Landscape>
+  let vm: any
+
+  beforeAll(() => {
+    view = shallowMount(Landscape)
+    vm = (view as any).vm
+  })
+
+  afterAll(() => {
+    view.destroy()
+  })
+
+  describe('season', () => {
+    describe('default', () => {
+      it('should show the fall illustration', () => {
+        expect(vm.img).toEqual('@/assets/img/illustration_1.jpg')
+      })
+    })
+    describe('fall', () => {
+      it('should show the fall illustration', () => {
+        view.setProps({
+          season: 'fall'
+        })
+        expect(vm.img).toEqual('@/assets/img/illustration_1.jpg')
+      })
+    })
+    describe('winter', () => {
+      it('should show the winter illustration', () => {
+        view.setProps({
+          season: 'winter'
+        })
+        expect(vm.img).toEqual('@/assets/img/illustration_2.jpg')
+      })
+    })
+  })
+})
